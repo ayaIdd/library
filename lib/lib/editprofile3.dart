e9b566be7a48b21f8ce89e1de0b66467ff7cf8eb
ayaIdd <koukssgirl@gmail.com>
2022-05-14 03:10:40 +0100

    modifications

--- lib/lib/editprofile3.dart
+++ lib/lib/editprofile3.dart
@@ -32,6 +32,8 @@ class _EditProfileUIState extends State<EditProfileUI> {
 
   bool _firstname = true;
   bool _secondname= true;
+  bool _number= true;
+  bool _gender= true;
   bool _email= true;
   String name;
 
@@ -39,271 +41,317 @@ class _EditProfileUIState extends State<EditProfileUI> {
   final FirebaseAuth auth= FirebaseAuth.instance;
   final EMAIL = FirebaseAuth.instance.currentUser.email ;
 
-   bool isObscurePassword = true;
+  bool isObscurePassword = true;
 
-   @override
-  void initState() {
+  @override
+  // void initState() {
+  //   super.initState();
+  //   getUser();
+  // }
+  //
+  // // getUser() async{
+  //   setState(() {
+  //     isLoading=true;
+  //   });
+  //   DocumentSnapshot documentSnapshot =
+  //   await FirebaseFirestore.instance
+  //       .collection('users')
+  //       .doc(( FirebaseAuth.instance.currentUser).uid).get();
+  //   UserModel user  = documentSnapshot.data();
+  //
+  //
+  // }
+  //
+  String familyName = "prénom loading ...";
+  String emaill = "email loading ...";
+  String firstName = "nom loading ...";
+  String phone = "numéro de téléphone   loading ...";
+  String genre = "genre loading ...";
+
+  void initState(){
+    getData();
     super.initState();
-    getUser();
   }
 
-  getUser() async{
-     setState(() {
-       isLoading=true;
-     });
-     DocumentSnapshot documentSnapshot =
-     await FirebaseFirestore.instance
-         .collection('users')
-         .doc(( FirebaseAuth.instance.currentUser).uid).get();
-     UserModel user  = documentSnapshot.data();
+  Future<void> getData() async {
+    User user =  await FirebaseAuth.instance.currentUser;
+    var vari = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
+    setState(() {
+      firstName = vari.data()['firstName'];
+       familyName = vari.data()['secondName'];
+     emaill = vari.data()['email'];
+     phone = vari.data()['phoneNumber'];
+     genre = vari.data()['genre'];
 
 
+    });
   }
-
 
 
   Widget build(BuildContext context) {
     return Scaffold(
 
         appBar: AppBar(
-        brightness: Brightness.dark,
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        toolbarHeight: 60,
-        title: Text('Mon Compte'),
+          brightness: Brightness.dark,
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          toolbarHeight: 60,
+          title: Text('Mon Compte'),
           centerTitle: true,
 
-        flexibleSpace:
+          flexibleSpace:
 
-            Container(
-              decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(bottomLeft:  Radius.circular(40), bottomRight: Radius.circular(40)),
-              gradient: LinearGradient(
-              colors: [Color(0xFFE59a59),Color(0xFFE59a59)],
-              begin: Alignment.bottomCenter,
-              end: Alignment.topCenter,
-            )
-           ),
-           ),
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(bottomLeft:  Radius.circular(40), bottomRight: Radius.circular(40)),
+                gradient: LinearGradient(
+                  colors: [Color(0xFFE59a59),Color(0xFFE59a59)],
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                )
+            ),
+          ),
 
 
-           leading: IconButton(
-             icon: Icon(
+          leading: IconButton(
+            icon: Icon(
               Icons.arrow_back,
-               color: Colors.white,
+              color: Colors.white,
             ),
 
-          onPressed: () {
-            Navigator.push(context ,MaterialPageRoute(builder: (context)=> ProfileUI()));
-          } ,
-             ),
-
+            onPressed: () {
+              Navigator.push(context ,MaterialPageRoute(builder: (context)=> ProfileUI()));
+            } ,
           ),
 
-    body:
+        ),
 
+        body:
 
-    Container(
-      decoration: new BoxDecoration(color: Colors.white70),
-      padding: EdgeInsets.only(left: 15 , top: 20 , right : 15),
-      child: GestureDetector(
-      onTap: (){
-      FocusScope.of(context).unfocus();
-      user=getUser();
-
-    },
-
-        child: ListView(
-
-        children: [
-        Center(
-        child: Stack(
-
-        children: [
 
         Container(
-            width : 130,
-            height : 130,
-            decoration: BoxDecoration(
-            border: Border.all(width: 4, color: Colors.white),
-            boxShadow: [
-                BoxShadow(
-                  spreadRadius: 2,
-                  blurRadius: 10,
-                  color : Colors.black.withOpacity(0.1),
-                  ),
-            ],
+            decoration: new BoxDecoration(color: Colors.white70),
+            padding: EdgeInsets.only(left: 15 , top: 20 , right : 15),
+            child: GestureDetector(
+                onTap: (){
+                  FocusScope.of(context).unfocus();
 
-        shape : BoxShape.circle,
-            image: DecorationImage(
-            fit: BoxFit.cover,
-            image: NetworkImage('https://media.istockphoto.com/photos/colored-powder-explosion-on-black-background-picture-id1140180560?k=20&m=1140180560&s=612x612&w=0&h=X_400OQDFQGqccORnKt2PHYvTZ3dBLeEnCH_hRiUQrY='),
+                },
 
-        ),
+                child: ListView(
 
+                  children: [
+                    Center(
+                      child: Stack(
 
-        ),
-        ),
+                        children: [
 
+                          Container(
+                            width : 130,
+                            height : 130,
+                            decoration: BoxDecoration(
+                              border: Border.all(width: 4, color: Colors.white),
+                              boxShadow: [
+                                BoxShadow(
+                                  spreadRadius: 2,
+                                  blurRadius: 10,
+                                  color : Colors.black.withOpacity(0.1),
+                                ),
+                              ],
 
+                              shape : BoxShape.circle,
+                              image: DecorationImage(
+                                fit: BoxFit.cover,
+                                image: NetworkImage('https://media.istockphoto.com/photos/colored-powder-explosion-on-black-background-picture-id1140180560?k=20&m=1140180560&s=612x612&w=0&h=X_400OQDFQGqccORnKt2PHYvTZ3dBLeEnCH_hRiUQrY='),
 
-
-        Positioned(
-            bottom: 0,
-            right: 0,
-            child: Container(
-              height: 40,
-              width: 40,
-              decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              border: Border.all(
-              width: 4,
-              color: Colors.white,
-
-               ),
-              color: Color(0xFF2661FA),
-              ),
-              child: Icon(
-              Icons.edit,
-              color: Colors.white,
-              ),
+                              ),
 
 
-        ),
+                            ),
+                          ),
 
 
 
 
+                          Positioned(
+                            bottom: 0,
+                            right: 0,
+                            child: Container(
+                              height: 40,
+                              width: 40,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                border: Border.all(
+                                  width: 4,
+                                  color: Colors.white,
 
-        ),
-
-        ],
-        ),
-        ),
-
-
-              SizedBox(height: 10),
-
+                                ),
+                                color: Color(0xFF2661FA),
+                              ),
+                              child: Icon(
+                                Icons.edit,
+                                color: Colors.white,
+                              ),
 
 
-                TextField(
-                  controller:firstnameController,
-                  decoration: decoration(( FirebaseAuth.instance.currentUser).uid),
-
-                ),
-                const SizedBox(height:24),
-                TextField(
-                  controller:secondnameController,
-                  decoration: decoration(('second' )),
-
-                ),
-                 const SizedBox(height:24),
-                TextField(
-                  controller:emailController,
-                  decoration: decoration(( FirebaseAuth.instance.currentUser).email),
-
-                ),
-                const SizedBox(height:24),
-          TextField(
-            controller:numberController,
-            decoration: decoration("number"),
-
-          ),
-          const SizedBox(height:24),
-          TextField(
-            controller:passwordController,
-            decoration: decoration("tap ONLY if you forgot password"),
-                //   onTap:  () {
-                // FirebaseAuth.instance.sendPasswordResetEmail(email:EMAIL) ;
-                // Navigator.of(context).pop();
-                // },
-            
-
-           //   ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: const Text("Profile updated!")));
+                            ),
 
 
 
-    ),
-          const SizedBox(height:24),
-          TextField(
-            controller:genreController,
-            decoration: decoration(" genre  : :F/H"),
-
-          ),
-          const SizedBox(height:24),
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  OutlineButton(
-                    onPressed: (){},
-                    child: Text("Annuler", style: TextStyle(
-
-                      fontSize: 15,
-                      letterSpacing: 2,
-                      color: Colors.black,
-                       fontWeight: FontWeight.bold
-                    )),
-
-                  ),
-
-                  ElevatedButton(
-                      onPressed: () { updateProfileData();},
-                      child: Text("Enregistrer", style: TextStyle(
-                          fontSize: 15,
-                          letterSpacing: 2,
-                          color: Colors.white,
-
-                      )),
-
-                  style: ElevatedButton.styleFrom(
-                    primary: Color(0xFF2661FA),
-                    padding: EdgeInsets.symmetric(horizontal: 50),
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-                  )
 
 
-                  ),
-                ]
+                          ),
+
+                        ],
+                      ),
+                    ),
 
 
-              ),
-            ],
-    ))));
+                    SizedBox(height: 10),
+
+
+
+                    TextField(
+                      controller:firstnameController,
+                          decoration: decoration('nom $firstName'),
+
+                    ),
+                    const SizedBox(height:24),
+                    TextField(
+                      controller:secondnameController,
+                      decoration: decoration('prenom $familyName '),
+
+                    ),
+                    const SizedBox(height:24),
+                    TextField(
+                      controller:emailController,
+                      decoration: decoration('email $emaill'),    // FirebaseAuth.instance.currentUser).email
+
+                    ),
+                    const SizedBox(height:24),
+                    TextField(
+                      controller:numberController,
+                      decoration: decoration(" num tel  ${phone}"),
+
+
+                    ),
+                    const SizedBox(height:24),
+
+
+                    TextField(
+                      controller:genreController,
+                      decoration: decoration(" genre  : ${genre}"),
+                    ),
+                    const SizedBox(height:24),
+
+
+                    TextButton(
+                      child : Text("Mot de passe oublier ?  "),
+                        style: TextButton.styleFrom(
+                          primary: Colors.redAccent,
+                          onSurface: Colors.black,
+                          padding: EdgeInsets.only(left: 10, right:10),
+
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(100)),
+                            side: BorderSide(color: Colors.black, width: 2),
+                          ),
+
+                          // shape: RoundedRectangleBorder(
+                          //               side: BorderSide(color: Colors.red, width: 2),
+                          //             ),
+
+                        ),
+                      onPressed:  () {
+                        FirebaseAuth.instance.sendPasswordResetEmail(email:EMAIL) ;
+                         ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: const Text("Un email été envoyer à votre compte ")));
+                      }
+                    ),
+                    const SizedBox(height:24),
+                    SizedBox(height: 10),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          OutlineButton(
+                            onPressed: (){},
+                            child: Text("Annuler", style: TextStyle(
+
+                                fontSize: 15,
+                                letterSpacing: 2,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold
+                            )),
+
+                          ),
+
+                          ElevatedButton(
+                              onPressed: () { updateProfileData();},
+                              child: Text("Enregistrer", style: TextStyle(
+                                fontSize: 15,
+                                letterSpacing: 2,
+                                color: Colors.white,
+
+                              )),
+
+                              style: ElevatedButton.styleFrom(
+                                primary: Color(0xFF2661FA),
+                                padding: EdgeInsets.symmetric(horizontal: 50),
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                              )
+
+
+                          ),
+                        ]
+
+
+                    ),
+                  ],
+                ))));
   }
   updateProfileData() {
     setState(() {
       firstnameController.text.trim().length < 3 ||
-              firstnameController.text.isEmpty
+          firstnameController.text.isEmpty
           ? _firstname = false
           : _firstname = true;
       secondnameController.text.trim().length < 3 ||
-              secondnameController.text.isEmpty
+          secondnameController.text.isEmpty
           ? _secondname = false
           : _secondname = true;
       emailController.text.trim().length < 3 ||
-              emailController.text.isEmpty
+          emailController.text.isEmpty
           ? _email = false
-          : _email = true;//usersRef.document(widget.currentUserId).updateData
+          : _email = true;
+      numberController.text.trim().length < 3 ||
+          numberController.text.isEmpty
+          ? _number = false
+          : _number = true;
+
+      genreController.text.trim().length < 3 ||
+          genreController.text.isEmpty
+      ? _gender = false
+          : _gender = true;
+      //usersRef.document(widget.currentUserId).updateData
     });
 
-    if (_firstname && _secondname && _email) {
+    if (_firstname  && _secondname && _email && _gender && _number ) {
 
 
       FirebaseFirestore.instance
-            .collection('users')
-            .doc(( FirebaseAuth.instance.currentUser).uid)
-            .update({
+          .collection('users')
+          .doc(( FirebaseAuth.instance.currentUser).uid)
+          .update({
         "firstName": firstnameController.text,
         "secondName": secondnameController.text,
         "email": emailController.text,
         "phoneNumber" : numberController.text,
         "genre": genreController.text,
       });
-     ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: const Text("Profile updated!")));
-     //  SnackBar snackbar = SnackBar(content: Text("Profile updated!"));
-     //      ScaffoldMessenger.(snackBar);
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: const Text("Profile mis à jour")));
+      //  SnackBar snackbar = SnackBar(content: Text("Profile updated!"));
+      //      ScaffoldMessenger.(snackBar);
     }
+    else ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: const Text("Remplissez tout les champs !")));
   }
 
   Widget buildTextField(String labelText, String placeholder, bool isPasswordTextField){
@@ -343,3 +391,4 @@ class _EditProfileUIState extends State<EditProfileUI> {
 
 
 }
+
